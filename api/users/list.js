9ea2131653826926fee0a431f285600d0bcf315b
chrisha <153200985+cchrisha@users.noreply.github.com>
2026-01-19 14:04:53 +0800

    first commit

--- api/users/list.js
+++ api/users/list.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   try {
     await dbConnect();
 
-    const user = auth(req);
+    const admin = auth(req);
     allow(user, "admin");
 
     const users = await User.find()
