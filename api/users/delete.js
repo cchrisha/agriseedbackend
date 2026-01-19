1a3115809009210a6306ed8eb5102101a492a82c
chrisha <153200985+cchrisha@users.noreply.github.com>
2026-01-19 16:20:06 +0800

    first commit

--- api/users/delete.js
+++ api/users/delete.js
@@ -8,17 +8,17 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: "Method not allowed" });
 
   try {
-await dbConnect();
+    await dbConnect();
 
-const admin = auth(req);
-if (!admin || !allow(admin, "admin")) {
-  return res.status(401).json({ message: "Unauthorized" });
-}
+  const user = auth(req);
+  if (!user || !allow(user, "admin")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
     const { userId } = req.body;
 
-    const user = await User.findByIdAndDelete(userId);
-    if (!user)
+    const targetUser = await User.findByIdAndDelete(userId);
+    if (!targetUser)
       return res.status(404).json({ message: "User not found" });
 
     res.json({ message: "User deleted successfully" });
