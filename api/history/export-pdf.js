57e4c3aa0ff3ea08cf084d65992bb6570bbd6ed6
chrisha <153200985+cchrisha@users.noreply.github.com>
2026-02-23 14:48:28 +0800

    “commit”

--- api/history/export-pdf.js
+++ api/history/export-pdf.js
@@ -55,11 +55,11 @@ try {
 doc
   .font("Helvetica-Bold")
   .fontSize(16)
-  .text("DEPARTMENT OF AGRICULTURE", 0, 100, { align: "center" });
+  .text("DEPARTMENT OF AGRICULTURE", { align: "center" });
 
 doc
   .fontSize(14)
-  .text("PREC STA. BARBARA", 0, 50, { align: "center" });
+  .text("PREC STA. BARBARA", { align: "center" });
 
 doc.moveDown(1);
 
