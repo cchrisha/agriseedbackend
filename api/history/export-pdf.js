5edc3456aae4e9ca96df1b59905dab255e5df609
chrisha <153200985+cchrisha@users.noreply.github.com>
2026-02-23 14:17:27 +0800

    “commit”

--- api/history/export-pdf.js
+++ api/history/export-pdf.js
@@ -1,5 +1,6 @@
 import dbConnect from "../../lib/db.js";
 import SeedStock from "../../models/SeedStock.js";
+import Seed from "../../models/Seed.js";
 import PDFDocument from "pdfkit";
 
 export default async function handler(req, res) {
