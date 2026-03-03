8f65cb73ec2369fa89bd0cb55b2cbd049228488b
chrisha <153200985+cchrisha@users.noreply.github.com>
2026-03-03 14:38:58 +0800

    “commit”

--- models/LegendaryBlock.js
+++ models/LegendaryBlock.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const LegendaryBlockSchema = new mongoose.Schema({
   name: String,
+  blockNumber: { type: Number, required: true, unique: true }, // 👈 ADD THIS
   lots: { type: Number, default: 2 },
   slotsPerLot: { type: Number, default: 200 },
 }, { timestamps: true });
